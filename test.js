23994c0b8c3cb3afb36b7421954e1f6342000d18
Bernadette St.George <bernadette.stgeorge@gmail.com>
2018-07-04 15:22:36 -0600

    Expresses success.

--- test.js
+++ test.js
@@ -1,17 +1,42 @@
+/*jslint
+    devel, browser
+*/
 import parseq from "./parseq.js";
-
-parseq.sequence([inviteRequestor], 3000)(testRequest);
 
 function testRequest(value, reason) {
     if (reason) {
         console.error(reason);
     }
-    console.assert(value.id === "430758941530783754", "Not the Imaginary Jobs Discord widget.");
-    console.assert(value.responseURL === "https://discordapp.com/api/guilds/430758941530783754/widget.json", "Bad responseURL.");
-    console.assert(value.status === 200, "Failed to receive correct response status.");
+
+
+    const ijpDiscordID = "430758941530783754";
+
+    const ijpDiscordUrl =
+            "https://discordapp.com/api/guilds/430758941530783754/widget.json";
+
+    const assertions = {
+        "Not the Imaginary Jobs Discord widget.": value.id === ijpDiscordID,
+        "Bad responseURL.": value.responseURL === ijpDiscordUrl,
+        "Failed to receive correct response status.": value.status === 200
+
+    };
+
+    const failingAssertions =
+            Object.keys(assertions).filter(
+        (assertion) => !assertions[assertion]
+    );
+
+    if (failingAssertions.length === 0) {
+        console.log("All tests are passing");
+    }
+
+    if (failingAssertions.length !== 0) {
+        console.error(failingAssertions);
+    }
+
 }
 
-function inviteRequestor(callback, value) {
+function inviteRequestor(callback) {
     const invitationRequest = new XMLHttpRequest();
     invitationRequest.addEventListener("load", function successHandler(event) {
         const {id} = JSON.parse(event.target.responseText);
@@ -21,10 +46,13 @@ function inviteRequestor(callback, value) {
             status: event.target.status
         });
     });
-    invitationRequest.open("GET", "https://discordapp.com/api/guilds/430758941530783754/widget.json");
+    invitationRequest.open(
+        "GET",
+        "https://discordapp.com/api/guilds/430758941530783754/widget.json"
+    );
     invitationRequest.send();
 }
 
 
-
+parseq.sequence([inviteRequestor], 3000)(testRequest);
 
